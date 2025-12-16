f4465e1d7f51a531319738efa8728ea26ef3959d
dylanvanzanten <info@dylanvanzanten.nl>
2025-12-16 14:18:31 +0100

    feat(): add next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+import createNextIntlPlugin from "next-intl/plugin";
+
 const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
@@ -14,4 +16,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+export default withNextIntl(nextConfig);
